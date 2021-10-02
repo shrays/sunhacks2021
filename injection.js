cf286f1cbdea8770d031db29124983a593bfba09
harshavb <48032800+harshavb@users.noreply.github.com>
2021-10-02 13:49:41 -0700

    Add server.js to start listening to websocket

--- injection.js
+++ injection.js
@@ -2,4 +2,24 @@ window.onload = function() {
 	$('input').on('click', function() {
 		$(this).val("bruh");
 	});
+	
+	let isRecording = false;
+	let socket;
+	let recorder;
+	
+	const run = async () => {
+		if (isRecording) {
+			if (socket) {
+				socket.send(JSON.stringify({terminate_session: true}));
+				socket.close();
+				socket = null;
+			}
+			
+			if (recorder) {
+				recorder.pauseRecording();
+				recorder = null;
+			}
+		} else {
+		}
+	}
 }
