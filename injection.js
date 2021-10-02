083a4aceb69d90b03be6946cb291e7fd964f2637
harshavb <48032800+harshavb@users.noreply.github.com>
2021-10-02 14:04:37 -0700

    get everything working?

--- injection.js
+++ injection.js
@@ -1,8 +1,5 @@
 window.onload = function() {
-	$('input').on('click', function() {
-		$(this).val("bruh");
-	});
-	
+	let textBox;
 	let isRecording = false;
 	let socket;
 	let recorder;
@@ -20,6 +17,73 @@ window.onload = function() {
 				recorder = null;
 			}
 		} else {
+			const response = await fetch('http://localhost:5000'); // get temp session token from server.js (backend)
+			const data = await response.json();
+			const { token } = data;
+			
+			socket = await new WebSocket(`wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000&token=${token}`); // establish wss with AssemblyAI (AAI) at 16000 sample rate
+			
+			socket.onerror = (event) => console.error('Error:', event);
+			socket.onclose = event => {
+				console.log(event)
+			}
+			
+			// handle incoming messages to display transcription to the DOM
+			const texts = {};
+			socket.onmessage = (message) => {
+				let msg = '';
+				const res = JSON.parse(message.data);
+				texts[res.audio_start] = res.text;
+				const keys = Object.keys(texts);
+				keys.sort((a, b) => a - b);
+				for (const key of keys) {
+					if (texts[key]) {
+						msg += ` ${texts[key]}`;
+					}
+				}
+				textBox.val(msg);
+			};
+			
+			socket.onopen = () => {
+			  // once socket is open, begin recording
+				messageEl.style.display = '';
+				navigator.mediaDevices.getUserMedia({ audio: true })
+					.then((stream) => {
+						recorder = new RecordRTC(stream, {
+						type: 'audio',
+						mimeType: 'audio/webm;codecs=pcm', // endpoint requires 16bit PCM audio
+						recorderType: StereoAudioRecorder,
+						timeSlice: 250, // set 250 ms intervals of data that sends to AAI
+						desiredSampRate: 16000,
+						numberOfAudioChannels: 1, // real-time requires only one channel
+						bufferSize: 4096,
+						audioBitsPerSecond: 128000,
+						ondataavailable: (blob) => {
+							const reader = new FileReader();
+							reader.onload = () => {
+								const base64data = reader.result;
+
+								// audio data must be sent as a base64 encoded string
+								if (socket) {
+									socket.send(JSON.stringify({ audio_data: base64data.split('base64,')[1] }));
+								}
+							};
+							reader.readAsDataURL(blob);
+						},
+					});
+
+					recorder.startRecording();
+				})
+				.catch((err) => console.error(err));
+			};
 		}
-	}
+	};
+	isRecording = !isRecording;
+	buttonEl.innerText = isRecording ? 'Stop' : 'Record';
+	titleEl.innerText = isRecording ? 'Click stop to end recording!' : 'Click start to begin recording!'
+	
+	$('input').on('click', function() {
+		textBox = $(this);
+		run();
+	});
 }
